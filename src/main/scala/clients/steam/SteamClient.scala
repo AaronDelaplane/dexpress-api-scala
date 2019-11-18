02539c783b6a082cfffa371c256ecb242c9ba001
Aaron Delaplane <delaplaneaaron@gmail.com>
2019-11-17 20:50:38 -0500

    clients.steam: shorten error messages

--- src/main/scala/clients/steam/SteamClient.scala
+++ src/main/scala/clients/steam/SteamClient.scala
@@ -21,34 +21,33 @@ class SteamClient(config: SteamClientConfig, httpClient: Client[IO]) extends Htt
     st.localized_tag_name
       .fold[IO[String]](IO.raiseError(new Exception("localized_tag_name not defined")))(IO.pure)
   
+    def attemptTag(tags: NonEmptyList[SteamTag], tagName: String): IO[String] =
+      tags
+        .find(_.category.map(_.toLowerCase === tagName).getOrElse(false))
+        .fold[IO[SteamTag]](IO.raiseError(new Exception(s"tag $tagName not found")))(IO.pure)
+        .flatMap[String](attemptLocalizedTagName)
+  
   def attemptOptionalTag(tags: NonEmptyList[SteamTag], tagName: String): IO[Option[String]] =
     tags
       .find(_.category.map(_.toLowerCase === tagName).getOrElse(false))
       .fold[IO[Option[String]]](IO.pure(None))(attemptLocalizedTagName(_).map(Some.apply))
   
-  def attemptRequiredTag(tags: NonEmptyList[SteamTag], tagName: String): IO[String] =
-    tags
-      .find(_.category.map(_.toLowerCase === tagName).getOrElse(false))
-      .fold[IO[SteamTag]](IO.raiseError(new Exception(s"tag $tagName not found")))(IO.pure)
-      .flatMap[String](attemptLocalizedTagName)
-    
-  
   def attemptToAsset(a: SteamAsset)(d: SteamDescription): IO[Asset] =
     for {
-      appid            <- a.appid.fold[IO[Int]](IO.raiseError(new Exception("appid is not defined")))(IO.pure)
-      assetid          <- a.assetid.fold[IO[String]](IO.raiseError(new Exception("assetid is not defined")))(IO.pure)
-      classid          <- a.classid.fold[IO[String]](IO.raiseError(new Exception("classid is not defined")))(IO.pure)
-      instanceid       <- a.instanceid.fold[IO[String]](IO.raiseError(new Exception("instanceid is not defined")))(IO.pure)
-      tradable         <- d.tradable.fold[IO[Int]](IO.raiseError(new Exception("tradable is not defined")))(IO.pure)
-      market_hash_name <- d.market_hash_name.fold[IO[String]](IO.raiseError(new Exception("market_hash_name is not defined")))(IO.pure)
-      icon_url         <- d.icon_url.fold[IO[String]](IO.raiseError(new Exception("icon_url is not defined")))(IO.pure)
-      asset_type       <- d.`type`.fold[IO[String]](IO.raiseError(new Exception("type is not defined")))(IO.pure)
+      appid            <- a.appid.fold[IO[Int]](IO.raiseError(new Exception("appid not defined")))(IO.pure)
+      assetid          <- a.assetid.fold[IO[String]](IO.raiseError(new Exception("assetid not defined")))(IO.pure)
+      classid          <- a.classid.fold[IO[String]](IO.raiseError(new Exception("classid not defined")))(IO.pure)
+      instanceid       <- a.instanceid.fold[IO[String]](IO.raiseError(new Exception("instanceid not defined")))(IO.pure)
+      tradable         <- d.tradable.fold[IO[Int]](IO.raiseError(new Exception("tradable not defined")))(IO.pure)
+      market_hash_name <- d.market_hash_name.fold[IO[String]](IO.raiseError(new Exception("market_hash_name not defined")))(IO.pure)
+      icon_url         <- d.icon_url.fold[IO[String]](IO.raiseError(new Exception("icon_url not defined")))(IO.pure)
+      asset_type       <- d.`type`.fold[IO[String]](IO.raiseError(new Exception("type not defined")))(IO.pure)
       item_data        <- IO.pure("item_data") // Some("-") // d.market_actions // transform to `item_data`
-      tags             <- d.tags.fold[IO[NonEmptyList[SteamTag]]](IO.raiseError(new Exception("tags is not defined")))(
+      tags             <- d.tags.fold[IO[NonEmptyList[SteamTag]]](IO.raiseError(new Exception("tags not defined")))(
                             NonEmptyList.fromList(_).fold[IO[NonEmptyList[SteamTag]]](IO.raiseError(new Exception("tags is empty")))(IO.pure)
                           )
       _ <- logger.info(tags.toString)
-      rarity           <- attemptRequiredTag(tags, "rarity")
+      rarity           <- attemptTag(tags, "rarity")
       exterior         <- attemptOptionalTag(tags, "exterior")
     } yield
       Asset(appid, assetid, classid, instanceid, tradable, market_hash_name, icon_url, asset_type, exterior, rarity, item_data)
